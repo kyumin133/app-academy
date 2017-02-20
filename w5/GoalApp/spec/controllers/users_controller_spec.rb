dc7d71e0538465e72ab97605ce6630fef33abe69
App Academy Student <student@appacademy.io>
2017-02-20 12:03:03 -0800

    Add user and user controller specs

---
+++ w5/GoalApp/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+
+
+  describe "GET #new" do
+    it "renders the new users page" do
+      get :new, user: {}
+
+      expect(response).to render_template("new")
+    end
+  end
+
+
+  describe "POST #create" do
+    it "redirects to the show user page if valid params" do
+      post :create, user: {username: "john", password: "password" }
+      expect(response).to redirect_to(users_url)
+    end
+
+    it "renders new users page if invalid params" do
+      post :create, user: {username: "samantha" }
+
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "GET #show" do
+    it "renders the show user page" do
+      User.create(username: "samantha", password: "password")
+      get :show, id: User.find_by(username: "samantha").id
+
+      expect(response).to render_template("show")
+    end
+  end
+
+end
---
+++ w5/GoalApp/spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) do
+    FactoryGirl.build(:user,
+      username: "jonathan",
+      password: "good_password",
+      cheers: 12)
+  end
+
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_length_of(:password).is_at_least(6) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_presence_of(:cheers) }
+
+  it { should have_many(:comments) }
+  it { should have_many(:goals) }
+
+  it "::find_by_credentials" do
+    new_user = User.create(username: "samantha", password: "password", cheers: 12)
+    user_find = User.find_by_credentials("samantha", "password")
+    expect(new_user.id).to eq(user_find.id)
+  end
+
+end
